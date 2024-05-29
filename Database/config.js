f4570b21b70e860c04f4ed0bd002096813d9c659
Prabhakar1116 <duraiprabhakar@gmail.com>
2024-05-30 00:07:50 +0530

    Publish

--- Database/config.js
+++ Database/config.js
@@ -1,23 +1,23 @@
-// importing necessary components
 import mongoose from "mongoose";
-import dotnev from "dotenv";
+import dotenv from "dotenv";
 
+dotenv.config();
 
-// ,iddleware
-dotnev.config();
+const mongodb_url = process.env.MONGODB_URL;
 
-// connecting to database
-const mongodb_url = process.env.MONGODB_URL
+const connectDB = async () => {
+    try {
+        if (!mongodb_url) {
+            throw new Error("MongoDB URL not set in environment variables");
+        }
+        await mongoose.connect(mongodb_url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("MongoDB Connected...");
+    } catch (error) {
+        console.error("MongoDB connection error:", error.message);
+    }
+};
 
-const connectDB = async()=>{
-    try{
-        const connection= await mongoose.connect(mongodb_url);
-            console.log("MongoDB Connected...")
-            }
-            catch(error){
-                console.log(error.message);
-                res.status(500).json({message:"MongoDB connection Error"})
-            }
-}
-
-export default connectDB
+export default connectDB;
